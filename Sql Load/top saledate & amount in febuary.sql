534011a9ef76115aed204f5387f786e7a3de6427
reddygautam98 <reddygautam98@gmail.com>
2024-08-12 21:56:11 +0530

    first

--- Sql Load/top saledate & amount in febuary.sql
+++ Sql Load/top saledate & amount in febuary.sql
@@ -1,7 +1,7 @@
 --top saledate & amount in febuary
 SELECT SaleDate, MAX(Amount) AS TopSalesAmount
 FROM sales 
-WHERE EXTRACT(MONTH FROM SaleDate) = 2
+WHERE EXTRACT(MONTH FROM SaleDate) = 1
 GROUP BY SaleDate
 ORDER BY TopSalesAmount DESC
-LIMIT 1;
+LIMIT 10;
